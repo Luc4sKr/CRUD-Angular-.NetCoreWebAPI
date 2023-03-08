88f5f74cc0222882fe5bb3eb326c67bd8b0466e8
Lucas <lucaskreuch77@gmail.com>
2023-03-08 14:42:39 -0300

    models | services

---
+++ AppAngular/src/app/services/persons.service.ts
@@ -0,0 +1,41 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from '../models/person';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "contentType": "application/json"
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonsService {
+  url = "https://localhost:7110/api/Person"
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.url);
+  }
+
+  getById(id: number): Observable<Person> {
+    const urlAPI = `${this.url}/${id}`;
+    return this.http.get<Person>(urlAPI);
+  }
+
+  save(person: Person): Observable<any> {
+    return this.http.post<Person>(this.url, person, httpOptions);
+  }
+
+  update(person: Person): Observable<any> {
+    return this.http.put<Person>(this.url, person, httpOptions);
+  }
+
+  delete(id: number): Observable<any> {
+    const urlAPI = `${this.url}/${id}`;
+    return this.http.delete<number>(urlAPI, httpOptions);
+  }
+}
